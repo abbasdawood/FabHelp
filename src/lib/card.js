43c50287465666b87c3be1c2441029342eaf67a5
Abbas Dawood <abbas11.it@gmail.com>
2018-11-23 16:55:36 +0530

    Capitalization of buttons

--- src/lib/card.js
+++ src/lib/card.js
@@ -47,7 +47,7 @@ export class Card {
             <div class="lds-ripple my-3"><div></div><div></div></div>
         </div>
         <div class="card-footer bg-white d-none" id="card-footer">
-            <button class="btn btn-sm btn-outline-primary" id="back-button">
+            <button class="btn btn-sm btn-outline-primary text-capitalize" id="back-button">
                 <i class="fa fa-sm fa-chevron-left mr-1"></i> Go Back
             </button>
         </div>
